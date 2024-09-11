916aeeb662cfc3db8a12ba14302ca4318987c9a5
sojib12 <sheikh.sojibt@gmail.com>
2024-09-11 15:42:29 +0200

    primary db creation

---
+++ database/db_scripts/SQLite_insert_scrpts.sql
@@ -0,0 +1,4 @@
+-- SQLite
+-- Insert sample data into the categories table
+INSERT INTO products (ProductName,ProductPrice,ProductCurrency, created_at, updated_at) VALUES
+    ('4K Minicam', 7000, 'EUR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
---
+++ database/migrations/2024_09_11_110222_create_product_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Product', function (Blueprint $table) {
+            $table->mediumIncrements('ProductID');
+            $table->string('ProductName', 255);             // by dafault not null
+            $table->decimal('ProductPrice', 8, 2)->nullable();
+            $table->string('ProductCurrency', 10)->default('EUR')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
