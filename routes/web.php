3ab09878b3b888f71d2b15ac7663a164049cf300
emc-avt-ilmenau-de <emc@avt-ilmenau.de>
2025-03-26 15:16:38 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::get('{locale?}', function ($locale = null) {
 
     // Fetch products using the controller
     $ProductController = new ProductController();
-    return $productController->index();
+    return $ProductController->index();
 })->name('home')->where('locale', 'en|de');
 
 // Other routes without locale constraints
