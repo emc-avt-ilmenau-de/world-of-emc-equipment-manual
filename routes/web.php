ba60bbe6776633001f3395ad5525a96ced55670f
emc-avt-ilmenau-de <emc@avt-ilmenau.de>
2025-03-27 01:49:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,10 @@ Route::middleware(['web', \App\Http\Middleware\LocaleMiddleware::class])->group(
     // Other routes...
 });
 
+Route::get('/debug-log', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
+
 
 /*// Default Route Handling
 Route::get('/{any}', function () {
